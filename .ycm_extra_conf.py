07e39f499300da4ef6b3f8b535e561b2480326b9
icoldplayer <slashsdull@gmail.com>
2019-12-08 05:54:53 +0700

    adding code styling using YouCompleteMe

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,5 @@
+def Settings( **kwargs ):
+  return {
+   #  'interpreter_path': '/home/icoldplayer/Documents/python/learning/notebook/env/lib64/python3.7/site-packages',
+    'interpreter_path': '/usr/bin/python3.7'
+  }
